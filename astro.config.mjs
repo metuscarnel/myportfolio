9f35d669e6c2643dbd291c502118315269e973e0
metus01 <carnelmetus@gmail.com>
2025-12-20 10:31:24 +0100

    Add URL configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://www.metuscarnel.fr', // ton nom de domaine
+  output: 'static', // très important pour le déploiement statique
+});
